ce605b95a43814b169f664ec23133f2846719cf7
Nedim Talha <by.siqmle@gmail.com>
2022-04-22 02:34:02 +0300

    v0.1.0

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb://127.0.0.1:27017';
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- index.js
+++ index.js
@@ -1,17 +1,65 @@
-var mysql = require('mysql');
-require('dotenv').config();
+const { config } = require('dotenv');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const passport = require('passport');
+const mongoose = require("mongoose");
+const flash = require('connect-flash');
+const session = require('express-session');
+const app = express();
+require('dotenv'),config();
 
-var con = mysql.createConnection({
-  host: `localhost:${process.env.DBPORT}`,
-  user: "simqle",
-  password: "Salnedo..886677"
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-});
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+
+
+var PORT = process.env.PORT;
+app.listen(PORT)
